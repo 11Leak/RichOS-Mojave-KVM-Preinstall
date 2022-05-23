95441f9f14b39290b348f6505b20d22eb3efdad2
11Leak <88968137+11Leak@users.noreply.github.com>
2022-05-23 14:43:15 +0700

    Update MacOS-Mojave.sh

--- MacOS-Mojave.sh
+++ MacOS-Mojave.sh
@@ -1,24 +1,22 @@
-echo "Checking Virtualization..."
-vtx=$(egrep -i '^flags.*(vmx|svm)' /proc/cpuinfo | wc -l)
-if [ $vtx = 0 ] ; then echo "[Error] Your PC Not Support VT-x / AMD-v. Press Ctrl+C to quit"&& sleep 2711
-fi
-echo Installing Software...
-sudo apt update -y
-sudo apt install wget git qemu-system qemu-utils python3 python3-pip -y 
-git clone https://github.com/foxlet/macOS-Simple-KVM.git
-cd macOS-Simple-KVM
-sudo python3  -m pip install click
-./jumpstart.sh --mojave
-echo "Downloading Mac OS Mojave Disk..." 
-wget -q --show-progress --no-check-certificate -O MyDisk.qcow2 https://tinyurl.com/9v4p8d5c
-cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
-availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
-availableRAM=$(echo $availableRAMcommand | bash)
-echo HEADLESS=1 MEM=$availableRAM"M" CPUS=$cpus SYSTEM_DISK=MyDisk.qcow2 ./headless.sh > silence.sh
-chmod +x silence.sh
-nohup ./silence.sh &>/dev/null &
-echo "All Done !!! Your Mac OS VM now Ready to use !!!"
-echo "Open VNC Viewer and connect with port 5900"
-echo "Your VM Specs: $availableRAM"M" / $cpus CPUs"
-echo "User: Mojave-User"
-echo "Pass: 123456"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+rm -rf ngrok-stable-linux-amd64.zip
+./ngrok authtoken 268tKkixRMBBxHKeBCpS6s72ohm_7UvxkT4VkysGcyKi2JpZN
+nohup ./ngrok tcp --region ap 5900 &>/dev/null &
+sudo apt-get update && apt-get install qemu -y
+sudo apt install qemu-utils -y
+sudo apt install qemu-system-x86-xen -y
+sudo apt install qemu-system-x86 -y
+wget -O MyDisk.qcow2 https://tinyurl.com/9v4p8d5c
+clear
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 \
+  -m 8G \
+  -cpu EPYC \
+  -boot order=c \
+  -drive file= MyDisk.qcow2,format=raw,if=virtio \
+  -drive file=virtio-win.iso,media=cdrom \
+  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+  -device usb-tablet \
+  -vnc :0 \
+  -smp cores=2 \
