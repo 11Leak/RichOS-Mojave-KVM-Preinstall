5459840450eb17abec3d30ba538ce520eff8b8a8
11Leak <88968137+11Leak@users.noreply.github.com>
2022-05-23 14:48:56 +0700

    Update MacOS-Mojave.sh

--- MacOS-Mojave.sh
+++ MacOS-Mojave.sh
@@ -7,14 +7,14 @@ sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
 sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
-wget -O MyDisk.qcow2 https://tinyurl.com/9v4p8d5c
+wget -O MyDisk.img https://tinyurl.com/9v4p8d5c
 clear
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
   -boot order=c \
-  -drive file= MyDisk.qcow2,format=raw,if=virtio \
+  -drive file= MyDisk.img,format=raw,if=virtio \
   -drive file=virtio-win.iso,media=cdrom \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
